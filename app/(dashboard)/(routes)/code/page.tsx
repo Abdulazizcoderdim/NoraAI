9772db090f78823d3912155abe34b870815ade73
AbdulazizRus <coderdim@gmail.com>
2024-04-15 21:05:53 +0500

    images

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -2,10 +2,11 @@
 
 import axios from 'axios';
 import * as z from "zod";
-import { MessageSquare } from "lucide-react";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
+import {ChatCompletionRequestMessage} from 'openai';
+import ReactMarkdown from 'react-markdown';
 
-import {ChatCompletionRequestMessage} from 'openai'
 import { Heading } from "@/components/heading";
 
 import { formSchema } from "./constants";
@@ -21,7 +22,7 @@ import { cn } from '@/lib/utils';
 import UserAvatar from '@/components/user-avatar';
 import BotAvatar from '@/components/bot-avatar';
 
-const ConversationPage = () => {
+const CodePage = () => {
     const router = useRouter()
     const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
 
@@ -42,7 +43,7 @@ const ConversationPage = () => {
          };  
          const newMessages = [...messages, userMessage]
 
-         const response = await axios.post("/api/conversation", {
+         const response = await axios.post("/api/code", {
           messages: newMessages,
          })
 
@@ -59,11 +60,11 @@ const ConversationPage = () => {
     return (
         <div>
             <Heading
-              title="Conversation"
-              description="Our most advanced conversation model."
-              icon={MessageSquare}
-              iconColor="text-violet-500"
-              bgColor="bg-violet-500/10"
+              title="Code Generation"
+              description="Generate code using description text."
+              icon={Code}
+              iconColor="text-green-700"
+              bgColor="bg-green-700/10"
             />
             <div className="px-4 lg:px-8">
                 <div>
@@ -78,7 +79,7 @@ const ConversationPage = () => {
                                 <FormItem className="col-span-12 lg:col-span-10">
                                      <FormControl className="m-0 p-0">
                                         <Input
-                                         placeholder="How do I calculate the radius of a circle?"
+                                         placeholder="Simple toggle button using react hooks."
                                          disabled={isLoading}
                                          {...field}
                                          className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
@@ -112,9 +113,23 @@ const ConversationPage = () => {
                         )}
                       >
                          {message.role === "user" ? <UserAvatar/> : <BotAvatar/>}
-                         <p className='text-sm'>
-                          {message.content}
-                         </p>
+                         
+                         <ReactMarkdown
+                           components={{
+                            pre: ({node, ...props}) => (
+                              <div className='overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg'>
+                                <pre {...props}/>   
+                              </div>
+                            ),
+                            code: ({node, ...props}) => (
+                              <code className='bg-black/10 rounded-lg p-1'
+                               {...props}/>
+                            )
+                           }}
+                           className="text-sm overflow-hidden leading-7"
+                         >
+                             {message.content || ""}
+                         </ReactMarkdown>
                       </div>
                      ))}
                   </div>
@@ -124,4 +139,4 @@ const ConversationPage = () => {
     );
 };
 
-export default ConversationPage;
+export default CodePage;
